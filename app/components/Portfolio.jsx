83816427f8cac3b735318e4b6684f4670baab8f5
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-10-27 01:43:40 +0600

    [on progress] portfolio

--- app/components/Portfolio.jsx
+++ app/components/Portfolio.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import Image from "next/image";
+import React from "react";
+import zaz from "../../public/zaz.png";
+import award from "../../public/award.png";
+import most from "../../public/most.png";
+import ladai from "../../public/ladai.png";
+import hestings from "../../public/hestings.png";
+import recites from "../../public/recites.png";
+import imgData from "../utils/portfolio.json";
+
 
 const Portfolio = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="testb h-margin space-y-10 p-4">
+      <div className="text-center space-y-3">
+        <p className="heading-2">Portfolio</p>
+        <p className="heading">Our Portfolio</p>
+      </div>
+
+    
+
+      <div className="grid grid-cols-3 gap-4">
+        {imgData?.map(({id, img, title, description}) => {
+          return (
+            <div className="col-span-1 testb" key={id}>
+              <div className="text-center space-y-4">
+                <div className="testb relative w-full h-[350px]">
+                  <Image
+                    className="object-contain"
+                    src={img}
+                    alt="model"
+                    fill
+                  />
+                </div>
+                <h3>{title}</h3>
+                <h3>{description}</h3>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
