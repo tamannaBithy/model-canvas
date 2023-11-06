2f0209b603d0cc57fbafb4566f996e5a17ca3330
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-11-06 19:11:40 +0600

    [in progress] experience section

--- app/components/Experience.jsx
+++ app/components/Experience.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import { BiSolidRightArrow } from "react-icons/bi";
 
 const Experience = () => {
   const imgData = [
@@ -46,26 +47,33 @@ const Experience = () => {
   ];
 
   return (
-    <div className="bg-[url('/video.png')]">
-      <div className="text-center py-20 space-y-10">
-        <div className="testb p-10">
-          <div class="w-20 h-20 rounded-full border border-blue-500 flex items-center justify-center">
-            <div class="w-10 h-10 rounded-full bg-white"></div>
+    <div className="bg-[url('/video.png')] bg-no-repeat">
+      <div className="text-center lg:py-24 py-10 lg:space-y-10 space-y-5">
+        <div className="mx-auto lg:w-52 lg:h-52 w-40 h-40 rounded-full border-4 border-white flex items-center justify-center">
+          <div
+            className="lg:w-32 lg:h-32 w-24 h-24 rounded-full bg-white hover:bg-secondary cursor-pointer transition-colors  delay-100
+            relative text-black hover:text-white"
+          >
+            <p className="text-center  lg:text-3xl text-2xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              <BiSolidRightArrow />
+            </p>
           </div>
         </div>
 
         <h2 className="heading">Our experience allows to bring disruptive</h2>
 
-        <div className="grid lg:grid-cols-5 px-32">
+        <div className="grid lg:grid-cols-5 grid-cols-2 lg:px-32">
           {imgData?.map(({ id, imgSrc }) => {
             return (
               <div
-                className={` ${id < 6 && "border-b border-b-[#ffffff14]"} ${
-                  id !== 5 && id !== 10 && "border-r border-r-[#ffffff14]"
+                className={` ${
+                  id < 6 && "lg:border-b lg:border-b-[#ffffff14]"
+                } ${
+                  id !== 5 && id !== 10 && "lg:border-r lg:border-r-[#ffffff14]"
                 } `}
                 key={id}
               >
-                <div className="col-span-1 relative h-[140px] w-[110px] mx-auto">
+                <div className="col-span-1 relative lg:h-[140px] h-[100px] w-[110px] mx-auto">
                   <Image
                     className="object-contain "
                     src={imgSrc}
