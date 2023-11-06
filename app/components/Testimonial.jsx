d1b71cec202c0cfa966dc335be0156b14e8319d2
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-11-06 22:44:45 +0600

    [in progress] testimonial json data created

--- app/components/Testimonial.jsx
+++ app/components/Testimonial.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React from "react";
 
 const Testimonial = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const data = [
+    {
+      id: 1,
+      name: "Huntur Dimitir",
+      designation: "Founder, Huntur Co.",
+      desc: "Huntur Dimitir includes some of the region most iconic landmarks, from super high-rise luxury developments, to five star hotels, to five star hotels Five star hospitals",
+      img: "",
+    },
+    {
+      id: 2,
+      name: "Fill Willam",
+      designation: "Marketer Huntur Co.",
+      desc: "Fill Willam includes some of the region most iconic landmarks, from super high-rise luxury developments, to five star hotels, to five star hotels hospitals For zone",
+      img: "",
+    },
+    {
+      id: 3,
+      name: "Dimitir Jac",
+      designation: "Founder, Huntur Co.",
+      desc: "Dimitir Jac includes some of the region most iconic landmarks, from super high-rise luxury developments, to five star hotels, to five star hotels Capital zone",
+      img: "",
+    },
+    {
+      id: 4,
+      name: "Huntur Dimitir",
+      designation: "Founder, Huntur Co.",
+      desc: "Our portfolio includes some of the region most iconic landmarks, from super high-rise luxury developments, to five star hotels, hospitals",
+      img: "",
+    },
+    {
+      id: 5,
+      name: "Jopez Filman",
+      designation: "Founder, Co Operate",
+      desc: "Jopez Filman includes some of the region most iconic landmarks, from super high-rise luxury developments, to five star hotels, to five star hotels for three Star",
+      img: "",
+    },
+    {
+      id: 6,
+      name: "Huntur Dimitir",
+      designation: "Founder, Huntur Co.",
+      desc: "Our portfolio includes some of the region most iconic landmarks, from super high-rise luxury developments, to five star hotels, hospitals",
+      img: "",
+    },
+  ];
+
+  return (
+    <div className="testb child-wrapper space-y-3">
+      <p className="heading-2">Testimonials</p>
+      <h2 className="heading">Our Happy Client Feedback</h2>
+
+      <div></div>
+    </div>
+  );
 };
 
 export default Testimonial;
