3789371010fddcd906c6108a45ce68d4a6d17ce9
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-11-07 18:57:35 +0600

    [in progress] testimonial scrolling

--- app/components/Testimonial.jsx
+++ app/components/Testimonial.jsx
@@ -1,57 +1,40 @@
+"use client";
+import Image from "next/image";
 import React from "react";
+import testimonialData from "../utils/testimonial.json";
+
+import ScrollContainer from "react-indiana-drag-scroll";
 
 const Testimonial = () => {
-  const data = [
-    {
-      id: 1,
-      name: "Huntur Dimitir",
-      designation: "Founder, Huntur Co.",
-      desc: "Huntur Dimitir includes some of the region most iconic landmarks, from super high-rise luxury developments, to five star hotels, to five star hotels Five star hospitals",
-      img: "",
-    },
-    {
-      id: 2,
-      name: "Fill Willam",
-      designation: "Marketer Huntur Co.",
-      desc: "Fill Willam includes some of the region most iconic landmarks, from super high-rise luxury developments, to five star hotels, to five star hotels hospitals For zone",
-      img: "",
-    },
-    {
-      id: 3,
-      name: "Dimitir Jac",
-      designation: "Founder, Huntur Co.",
-      desc: "Dimitir Jac includes some of the region most iconic landmarks, from super high-rise luxury developments, to five star hotels, to five star hotels Capital zone",
-      img: "",
-    },
-    {
-      id: 4,
-      name: "Huntur Dimitir",
-      designation: "Founder, Huntur Co.",
-      desc: "Our portfolio includes some of the region most iconic landmarks, from super high-rise luxury developments, to five star hotels, hospitals",
-      img: "",
-    },
-    {
-      id: 5,
-      name: "Jopez Filman",
-      designation: "Founder, Co Operate",
-      desc: "Jopez Filman includes some of the region most iconic landmarks, from super high-rise luxury developments, to five star hotels, to five star hotels for three Star",
-      img: "",
-    },
-    {
-      id: 6,
-      name: "Huntur Dimitir",
-      designation: "Founder, Huntur Co.",
-      desc: "Our portfolio includes some of the region most iconic landmarks, from super high-rise luxury developments, to five star hotels, hospitals",
-      img: "",
-    },
-  ];
-
   return (
-    <div className="testb child-wrapper space-y-3">
+    <div className="child-wrapper space-y-3 px-16 xl:px-0">
       <p className="heading-2">Testimonials</p>
       <h2 className="heading">Our Happy Client Feedback</h2>
 
-      <div></div>
+      <ScrollContainer>
+        <div className="grid lg:grid-cols-2 gap-9 pt-2">
+          {testimonialData?.map(({ id, name, designation, desc, img }) => {
+            return (
+              <div key={id}>
+                <div className="bg-[url('/testimonial/testimonial-bg.png')] bg-repeat px-8 lg:pt-10 py-7 space-y-6 lg:h-80 h-auto">
+                  <q className="tracking-wide lg:text-[18px] lg:leading-10 text-muted">
+                    {desc}
+                  </q>
+
+                  <div className="flex items-center gap-5">
+                    <Image src={img} alt="reviewer" width={64} height={64} />
+
+                    <div className="space-y-1">
+                      <p className="font-semibold tracking-wider">{name}</p>
+                      <p className="text-secondary text-sm">{designation}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </ScrollContainer>
     </div>
   );
 };
