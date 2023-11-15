37530aad72cf67db6cfded8e0e29f18d629387da
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2023-11-15 19:55:28 +0600

    [in progress] img issue

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React from "react";
+import heroimg1 from "../../public/heroImg/hero-1.png";
+import Image from "next/image";
+import line from "../../public/line.png";
+import Link from "next/link";
 
 const Hero = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="testb relative">
+      <div className="lg:h-auto h-[500px]">
+        <Image src={heroimg1} alt="heroImg" className="object-contain" />
+      </div>
+
+      <div className="absolute inset-0 bg-gradient-to-r from-black from-70% opacity-50"></div>
+
+      <div className="testb absolute bottom-1/2">
+        <Image src={line} alt="line" />
+        <h3>Alex D. Williamson</h3>
+        <h1 className="heading">
+          Creative <br /> Actor / Model
+        </h1>
+        <p>
+          Welcome to my portfolio, if you want, you can have one also, and it
+          can look like this.
+        </p>
+        <Link href="https://s7template.com/wp/ruposhi-new/contact/">
+          Contact Me{" "}
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
